432b03c9e9c076b4dbfd3aaf0070c1d4fe43dd02
matthiasvonarx <matthias.von-arx@gmx.ch>
2024-02-23 10:13:21 +0100

    fix typo

--- maettu-setup-lab-env.sh
+++ maettu-setup-lab-env.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -e
-
 if [ "$#" -eq 0 ]; then
     echo "Please supply the interface name"
     exit 1
 fi
 
-sudo ip addr add 192.168.137.100/24 dev $1
-sudo ip route add 192.168.137.0./24 dev $1
+sudo ip addr add 192.168.137.1/24 dev $1
+sudo ip route add 192.168.137.0/24 dev $1
