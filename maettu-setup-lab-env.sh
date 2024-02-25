8fba6fc4200a7fffe39faafce0073419fd24c7b9
matthiasvonarx <matthias.von-arx@gmx.ch>
2024-02-26 00:18:04 +0100

    update

--- maettu-setup-lab-env.sh
+++ maettu-setup-lab-env.sh
@@ -7,3 +7,6 @@ fi
 
 sudo ip addr add 192.168.137.1/24 dev $1
 sudo ip route add 192.168.137.0/24 dev $1
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o wlp1s0f0 -j MASQUERADE
+
