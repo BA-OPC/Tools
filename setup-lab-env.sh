32e90a6c70cd47ada9bd5bfaf15a6beb76a0e9f9
David Guler <david.r.guler@gmail.com>
2024-02-28 09:58:02 +0100

    fixed setup lab env script

--- setup-lab-env.sh
+++ setup-lab-env.sh
@@ -11,7 +11,7 @@ if [ $? -ne 0 ]; then
     fi
 
     nmcli connection add type ethernet ifname $1 ipv4.method shared con-name ba-lab
-    nmcli connection modify local ipv4.addresses 192.168.42.1/24
+    nmcli connection modify ba-lab ipv4.addresses 192.168.42.1/24
     nmcli connection up ba-lab
 fi
 
